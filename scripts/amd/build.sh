c52d867782e0438ea2bf0cf4a20057f8882e74ec
Michael Melesse <micmelesse@gmail.com>
2022-11-15 18:45:18 +0000

    add scripts
    update scripts
    
    ignore logs
    
    update scripts

---
+++ scripts/amd/build.sh
@@ -0,0 +1,11 @@
+set -x
+
+cd python
+pip uninstall -y triton
+
+export TRITON_USE_ROCM=ON
+# export TRITON_ROCM_DEBUG=ON
+export MI_GPU_ARCH=gfx90a
+
+pip install --verbose -e .
+pip install -U matplotlib pandas
