4d5260acf1d7c607ac8f77220824c11afdab910e
Michael Melesse <micmelesse@gmail.com>
2022-12-01 10:24:02 -0600

    ignore triton cache and update scripts

--- scripts/amd/test.sh
+++ scripts/amd/test.sh
@@ -36,15 +36,20 @@ sh scripts/amd/clean.sh
 # pytest -rfs --verbose python/tests/test_core_amd.py::test_bin_op[float32-float32-/] 2>&1 | tee /dockerx/triton/test_bin_op.log
 # pytest -rfs --verbose python/tests/test_core.py::test_load 2>&1 | tee /dockerx/triton/test_load.log
 
-export MLIR_ENABLE_DUMP=1
-export LLVM_IR_ENABLE_DUMP=1
-export AMDGCN_ENABLE_DUMP=1
+# export MLIR_ENABLE_DUMP=1
+# export LLVM_IR_ENABLE_DUMP=1
+# export AMDGCN_ENABLE_DUMP=1
 
 # python3 python/tutorials/01-vector-add.py
 # pytest -rfs --verbose "python/tests/test_vecadd.py"
+pytest -rfs --verbose "python/tests/test_vecadd.py::test_vecadd_fcmp_no_scf_masked[1-128-shape0]"
+
+
+# pytest -rfs --verbose "python/tests/test_vecadd.py::test_vecadd_no_scf[4-256-shape0]"
 # pytest -rfs --verbose "python/tests/test_vecadd.py::test_vecadd_scf_no_mask[4-256-1]"
 # pytest -rfs --verbose "python/tests/test_vecadd.py::test_vecadd_scf_mask[shape0-2-128-1]"
-pytest -rfs --verbose "python/tests/test_vecadd.py::test_vecadd_no_scf[4-256-shape0]"
+# pytest -rfs --verbose "python/tests/test_vecadd.py::test_vecadd_no_scf[4-256-shape0]"
+# pytest -rfs --verbose "python/tests/test_vecadd.py::test_vecadd_fcmp_no_scf_masked[4-256-shape3]"
 
 # python3 -m triton.tools.aot test/Target/tritongpu_to_ptx.mlir --target=amdgcn --gfx=906
 # pytest -rfs --verbose python/test/unit/language/test_core.py::test_masked_load[float32-128-2] \
