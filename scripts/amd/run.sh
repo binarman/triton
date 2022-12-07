7fdd90ff8cca51c1dafe3b152d1077ef18295276
Michael Melesse <micmelesse@gmail.com>
2022-12-07 07:07:21 -0600

    This is a combination of 6 commits.
    add scripts
    
    add vector load
    
    update unit tests path
    
    fix reduce 1d bug
    
    has skips on rocm
    
    clean up

---
+++ scripts/amd/run.sh
@@ -0,0 +1,17 @@
+clear
+
+set -x
+
+
+ROOT_DIR=$(pwd)
+LOG_DIR=$ROOT_DIR/log_$(git rev-parse --symbolic-full-name --abbrev-ref HEAD)
+rm -rf $LOG_DIR
+mkdir -p $LOG_DIR
+chmod -R 777 $LOG_DIR
+
+
+bash scripts/amd/clean.sh 2>&1 |tee $LOG_DIR/clean.log
+bash scripts/amd/build.sh 2>&1 |tee $LOG_DIR/build.log
+bash scripts/amd/test.sh  2>&1 |tee $LOG_DIR/test.log
+# bash scripts/amd/backtrace.sh 2>&1 |tee $LOG_DIR/backtrace.log
+# bash scripts/amd/cache_print.sh  2>&1 |tee $LOG_DIR/cache.log
---
+++ scripts/amd/test.sh
@@ -0,0 +1,2 @@
+# clear
+pytest -rfs --verbose python/test/unit/language/test_core.py | tee /dockerx/triton/test_core.log
