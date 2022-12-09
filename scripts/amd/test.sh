688aff74ff9a7c126e5dba8cf37e546485ce5b2d
Michael Melesse <micmelesse@gmail.com>
2022-12-08 20:18:11 -0600

    skip segfaults

--- scripts/amd/test.sh
+++ scripts/amd/test.sh
@@ -1,2 +1,6 @@
 # clear
-pytest -rfs --verbose python/test/unit/language/test_core.py | tee /dockerx/triton/test_core.log
+pytest -rfs --verbose python/test | tee /dockerx/triton/test_all.log
+# pytest -rfs --verbose python/test/unit/language/test_core.py | tee /dockerx/triton/test_core.log
+# pytest -rfs --verbose python/test/unit/language/test_dequantize.py | tee /dockerx/triton/test_dequantize.log
+# pytest -rfs --verbose python/test/unit/language/test_random.py | tee /dockerx/triton/test_random.log
+# pytest -rfs --verbose python/test/unit/operators/test_blocksparse.py | tee /dockerx/triton/test_blocksparse.log
