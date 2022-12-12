57158085df1027f22774847a90e0b9a6248481f4
Michael Melesse <micmelesse@gmail.com>
2022-12-12 09:01:13 -0600

    remove stuff not needed for phase 2

--- scripts/amd/test.sh
+++ scripts/amd/test.sh
@@ -15,7 +15,7 @@ pytest -rfs --verbose "python/tests/test_core_amd.py" 2>&1 | tee $LOG_DIR/test_c
 # pytest -rfs --verbose "python/tests/test_core.py::test_math_op" | tee $LOG_DIR/test_all.log
 pytest -rfs --verbose "python/tests/test_elementwise.py" 2>&1 | tee $LOG_DIR/test_elementwise.log
 pytest -rfs --verbose "python/tests/test_ext_elemwise.py" 2>&1 | tee $LOG_DIR/test_ext_elemwise.log
-pytest -rfs --verbose "python/tests/test_gemm.py" 2>&1 | tee $LOG_DIR/test_gemm.log
+# pytest -rfs --verbose "python/tests/test_gemm.py" 2>&1 | tee $LOG_DIR/test_gemm.log
 pytest -rfs --verbose "python/tests/test_reduce.py" 2>&1 | tee $LOG_DIR/test_reduce.log
 pytest -rfs --verbose "python/tests/test_transpose.py" 2>&1 | tee $LOG_DIR/test_transpose.log
 pytest -rfs --verbose "python/tests/test_vecadd.py" 2>&1 | tee $LOG_DIR/test_vecadd.log
