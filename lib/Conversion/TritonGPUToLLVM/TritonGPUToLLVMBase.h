0994f24fd52edb9678bcd28aa099704dd6355350
B1tway <andrew.shukshov@gmail.com>
2023-04-03 10:14:31 +0000

    [MFMA] Implementation of MFMA DotOp pipeline

--- lib/Conversion/TritonGPUToLLVM/TritonGPUToLLVMBase.h
+++ lib/Conversion/TritonGPUToLLVM/TritonGPUToLLVMBase.h
@@ -522,6 +522,10 @@ public:
           result = emitBaseIndexForMmaLayoutV1(loc, rewriter, mmaLayout, type);
         if (mmaLayout.isAmpere())
           result = emitBaseIndexForMmaLayoutV2(loc, rewriter, mmaLayout, type);
+#ifdef USE_ROCM
+        if (mmaLayout.isMI200())
+          llvm_unreachable("if (mmaLayout.isMI200()) not implemented");
+#endif
       } else {
         llvm_unreachable("unsupported emitBaseIndexForLayout");
       }
@@ -540,6 +544,10 @@ public:
         return emitOffsetForMmaLayoutV1(mmaLayout, type);
       if (mmaLayout.isAmpere())
         return emitOffsetForMmaLayoutV2(mmaLayout, type);
+#ifdef USE_ROCM
+      if (mmaLayout.isMI200())
+        llvm_unreachable("if (mmaLayout.isMI200()) not implemented");
+#endif
     }
     llvm_unreachable("unsupported emitOffsetForLayout");
   }
