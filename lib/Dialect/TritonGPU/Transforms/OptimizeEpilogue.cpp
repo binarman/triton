fbcc4d85bd99ab2edd14b2feea267469bdf1dc6d
Alexander Efimov <efimov.alexander@gmail.com>
2023-11-30 17:36:10 +0000

    fixxxx for atomicRMW support

--- lib/Dialect/TritonGPU/Transforms/OptimizeEpilogue.cpp
+++ lib/Dialect/TritonGPU/Transforms/OptimizeEpilogue.cpp
@@ -154,6 +154,139 @@ public:
   }
 };
 
+class BypassEpilogueRMWSMEM : public mlir::RewritePattern {
+
+public:
+  explicit BypassEpilogueRMWSMEM(mlir::MLIRContext *context)
+      : mlir::RewritePattern(triton::AtomicRMWOp::getOperationName(), 1, context) {}
+  mlir::LogicalResult
+  tryStore(mlir::Operation *op,
+                  mlir::PatternRewriter &rewriter) const {
+
+    auto stOp = dyn_cast<triton::StoreOp>(op);
+    if (!stOp)
+      return mlir::failure();
+    Value ptr = stOp.getPtr();
+    Value val = stOp.getValue();
+    Value mask = stOp.getMask();
+    auto ptrType = ptr.getType().dyn_cast<RankedTensorType>();
+    auto valType = val.getType().dyn_cast<RankedTensorType>();
+    if (!ptrType || !valType ||
+        !ptrType.getEncoding().isa<triton::gpu::BlockedEncodingAttr>() ||
+        !valType.getEncoding().isa<triton::gpu::BlockedEncodingAttr>())
+      return mlir::failure();
+
+    auto cvtOp = dyn_cast<triton::gpu::ConvertLayoutOp>(val.getDefiningOp());
+    if (!cvtOp)
+      return mlir::failure();
+
+    auto encoding =
+        cvtOp.getSrc().getType().cast<RankedTensorType>().getEncoding();
+
+#ifdef USE_ROCM
+    if (!encoding.isa<triton::gpu::MfmaEncodingAttr>())
+      return mlir::failure();
+#else
+    if (!encoding.isa<triton::gpu::MmaEncodingAttr>())
+      return mlir::failure();
+#endif
+
+    if (!cvtOp.getResult().hasOneUse())
+      return mlir::failure();
+
+    auto newEncoding =
+        cvtOp.getOperand().getType().cast<RankedTensorType>().getEncoding();
+
+    auto newVal = cvtOp.getOperand();
+
+    auto newPtrType = RankedTensorType::get(
+        ptrType.getShape(), ptrType.getElementType(), newEncoding);
+    Value newPtr = rewriter.create<triton::gpu::ConvertLayoutOp>(
+        ptr.getLoc(), newPtrType, ptr);
+
+    Value newMask = mask;
+    if (mask) {
+      auto maskType = mask.getType().dyn_cast<RankedTensorType>();
+      auto newMaskType = RankedTensorType::get(
+          maskType.getShape(), maskType.getElementType(), newEncoding);
+      newMask = rewriter.create<triton::gpu::ConvertLayoutOp>(
+          mask.getLoc(), newMaskType, mask);
+    }
+
+    rewriter.replaceOpWithNewOp<triton::StoreOp>(
+        stOp, newPtr, newVal, newMask, stOp.getCache(), stOp.getEvict());
+    return mlir::success();
+  }
+
+  mlir::LogicalResult
+  tryAtomicRMW(mlir::Operation *op,
+                  mlir::PatternRewriter &rewriter) const {
+
+    auto stOp = dyn_cast<triton::AtomicRMWOp>(op);
+    if (!stOp)
+      return mlir::failure();
+    Value ptr = stOp.getPtr();
+    Value val = stOp.getVal();
+    Value mask = stOp.getMask();
+    auto ptrType = ptr.getType().dyn_cast<RankedTensorType>();
+    auto valType = val.getType().dyn_cast<RankedTensorType>();
+    if (!ptrType || !valType ||
+        !ptrType.getEncoding().isa<triton::gpu::BlockedEncodingAttr>() ||
+        !valType.getEncoding().isa<triton::gpu::BlockedEncodingAttr>())
+      return mlir::failure();
+
+    auto cvtOp = dyn_cast<triton::gpu::ConvertLayoutOp>(val.getDefiningOp());
+    if (!cvtOp)
+      return mlir::failure();
+
+    auto encoding =
+        cvtOp.getSrc().getType().cast<RankedTensorType>().getEncoding();
+
+#ifdef USE_ROCM
+    if (!encoding.isa<triton::gpu::MfmaEncodingAttr>())
+      return mlir::failure();
+#else
+    if (!encoding.isa<triton::gpu::MmaEncodingAttr>())
+      return mlir::failure();
+#endif
+
+    if (!cvtOp.getResult().hasOneUse())
+      return mlir::failure();
+
+    auto newEncoding =
+        cvtOp.getOperand().getType().cast<RankedTensorType>().getEncoding();
+
+    auto newVal = cvtOp.getOperand();
+
+    auto newPtrType = RankedTensorType::get(
+        ptrType.getShape(), ptrType.getElementType(), newEncoding);
+    Value newPtr = rewriter.create<triton::gpu::ConvertLayoutOp>(
+        ptr.getLoc(), newPtrType, ptr);
+
+    Value newMask = mask;
+    if (mask) {
+      auto maskType = mask.getType().dyn_cast<RankedTensorType>();
+      auto newMaskType = RankedTensorType::get(
+          maskType.getShape(), maskType.getElementType(), newEncoding);
+      newMask = rewriter.create<triton::gpu::ConvertLayoutOp>(
+          mask.getLoc(), newMaskType, mask);
+    }
+
+    rewriter.replaceOpWithNewOp<triton::AtomicRMWOp>(
+        stOp, stOp.getResult().getType(), stOp.getAtomicRmwOpAttr(), newPtr, newVal, newMask, stOp.getSemAttr());
+    return mlir::success();
+  }
+
+  mlir::LogicalResult
+  matchAndRewrite(mlir::Operation *op,
+                  mlir::PatternRewriter &rewriter) const override {
+    auto res = tryStore(op, rewriter);
+    if (res.succeeded())
+      return res;
+    return tryAtomicRMW(op, rewriter);
+  }
+};
+
 } // namespace
 
 #define GEN_PASS_CLASSES
@@ -172,6 +305,7 @@ public:
     mlir::RewritePatternSet patterns(context);
 
     patterns.add<BypassEpilogueSMEM>(context);
+    patterns.add<BypassEpilogueRMWSMEM>(context);
 
     if (applyPatternsAndFoldGreedily(m, std::move(patterns)).failed()) {
       signalPassFailure();
