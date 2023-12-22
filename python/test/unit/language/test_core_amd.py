3e6ccb5318514ef3087121bb362f916649bb2184
Alexander Efimov <efimov.alexander@gmail.com>
2023-12-22 16:32:13 +0000

    fix

--- python/test/unit/language/test_core_amd.py
+++ python/test/unit/language/test_core_amd.py
@@ -2969,9 +2969,6 @@ else:
 @pytest.mark.parametrize("src_layout", layouts)
 @pytest.mark.parametrize("axis", [0, 1])
 def test_reduce_layouts(M, N, src_layout, axis, device='cuda'):
-    if torch.version.hip is not None and _get_warp_size() == 64:
-        if src_layout.is_transposed and axis == 0:
-            pytest.skip("Reduce along axis 0 is not supported in transposed mfma layout")
     if src_layout is MfmaLayout:
         if src_layout.m_dim < M or src_layout.n_dim < N:
             pytest.skip("incompatible MN dimensions")
