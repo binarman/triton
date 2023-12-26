02deded4703eac47ca947dceea15eaae1f6d5950
Alexander Efimov <efimov.alexander@gmail.com>
2023-12-26 14:29:51 +0000

    fix

--- lib/Conversion/TritonGPUToLLVM/ReduceOpToLLVM.cpp
+++ lib/Conversion/TritonGPUToLLVM/ReduceOpToLLVM.cpp
@@ -486,6 +486,7 @@ private:
         auto elemPtrTy = getElementPtrType(op, i);
         Value readPtr = gep(elemPtrTy, smemBases[i], readOffset);
         acc[i] = loadShared(rewriter, loc, readPtr, threadIsNeeded);
+        printValues(loc, rewriter, "final reduction load", {acc[i]});
       }
       warpReduce(rewriter, loc, acc, op, sizeInterWarps, 1 /* interleave */);
       // only the first thread in each sizeInterWarps is writing
