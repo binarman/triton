cc367c2848367fa32c4b53b6cf3ec25d6deff99a
Alexander Efimov <efimov.alexander@gmail.com>
2024-05-17 15:07:58 +0000

    use generator

--- python/test/unit/hopper/test_flashattention.py
+++ python/test/unit/hopper/test_flashattention.py
@@ -369,10 +369,13 @@ attention = _attention.apply
 ])
 @pytest.mark.skipif(torch.cuda.get_device_capability()[0] < 9, reason="requires arch 9+")
 def test_op(Z, H, N_CTX, D_HEAD, dtype=torch.float16):
-    torch.manual_seed(20)
-    q = torch.empty((Z, H, N_CTX, D_HEAD), dtype=dtype, device="cuda").normal_(mean=0.1, std=0.2).requires_grad_()
-    k = torch.empty((Z, H, N_CTX, D_HEAD), dtype=dtype, device="cuda").normal_(mean=0.4, std=0.2).requires_grad_()
-    v = torch.empty((Z, H, N_CTX, D_HEAD), dtype=dtype, device="cuda").normal_(mean=0.3, std=0.2).requires_grad_()
+    g = torch.manual_seed(20)
+    q = torch.empty((Z, H, N_CTX, D_HEAD), dtype=dtype, device="cuda").normal_(mean=0.1, std=0.2,
+                                                                               generator=g).requires_grad_()
+    k = torch.empty((Z, H, N_CTX, D_HEAD), dtype=dtype, device="cuda").normal_(mean=0.4, std=0.2,
+                                                                               generator=g).requires_grad_()
+    v = torch.empty((Z, H, N_CTX, D_HEAD), dtype=dtype, device="cuda").normal_(mean=0.3, std=0.2,
+                                                                               generator=g).requires_grad_()
     print("q input: ", q)
     print("k input: ", k)
     print("v input: ", v)
