bca57dc7d1846d7919187c224e479dd8e70eb2fc
Alexander Efimov <efimov.alexander@gmail.com>
2024-06-27 17:20:57 +0200

    Revert "address review comments" Revert "add todo for optimization investigation" Revert "Optimize code generated by EmitIndices for LinearLayout"

--- lib/Conversion/TritonGPUToLLVM/Utility.cpp
+++ lib/Conversion/TritonGPUToLLVM/Utility.cpp
@@ -231,30 +231,12 @@ emitIndicesUsingLinearLayouts(Location loc, RewriterBase &rewriter,
       withCTAOffset ? target.getClusterCTAId(rewriter, loc) : i32_val(0);
   unsigned rank = shape.size();
   SmallVector<SmallVector<Value>> ret;
-  // Linear layout function is split in two parts below:
-  // L(r, t, w, b) = L(0, t, w, b) xor L(r, 0, 0, 0)
-  //     idxs      =    idxsBase   xor    idxsReg
-  //
-  // L(0, t, w, b) part is same for all registers, nested out of reg loop
-  //
-  // This approach produces code with lower register pressure and
-  // less computations, compared to fused L(r,t,w,b) method.
-  auto idxsBase = applyLinearLayout(loc, rewriter, *ll,
-                                    {{kRegister, i32_val(0)},
-                                     {kLane, laneId},
-                                     {kWarp, warpId},
-                                     {kBlock, blockId}});
   for (unsigned reg = 0; reg < ll->getInDimSize(str_attr("register")); reg++) {
-    auto idxsReg =
-        ll->apply({{kRegister, reg}, {kLane, 0}, {kWarp, 0}, {kBlock, 0}});
-    SmallVector<std::pair<StringAttr, Value>> idxs;
-    for (auto [idxBase, idxReg] : llvm::zip(idxsBase, idxsReg)) {
-      auto dimName = idxBase.first;
-      assert(dimName == idxReg.first &&
-             "dim names of block+warp+thread and register idx should be equal");
-      auto idx = xor_(idxBase.second, i32_val(idxReg.second));
-      idxs.emplace_back(dimName, idx);
-    }
+    auto idxs = applyLinearLayout(loc, rewriter, *ll,
+                                  {{kRegister, i32_val(reg)},
+                                   {kLane, laneId},
+                                   {kWarp, warpId},
+                                   {kBlock, blockId}});
     assert(idxs.size() == rank);
     for (unsigned k = 0; k < rank; ++k) {
       assert(idxs[k].first == str_attr("dim" + std::to_string(k)));
