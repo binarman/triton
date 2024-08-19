77e211203579afc5cb02e6d16894a4b63ace8f63
Alexander Efimov <efimov.alexander@gmail.com>
2024-08-19 16:51:09 +0000

    Hoist reduction outside a loop
    This PR introduces an optimization that hoists reduction operation
    of dot accumulator outside a loop over K dimension:
    
      %acc = <zero tensor>
      for k tiles:
        %acc3d_input = reshape %acc
        %acc3d_out = dot3d(%x, %y, %acc3d_input)
        %acc = reduction batch %acc3d_out
    
    transforms to:
    
      %acc3d = <zero tensor>
      for k tiles:
        %acc3d = dot3d(%x, %y, %acc3d)
      %acc = reduction batch %acc3d

--- bin/RegisterTritonDialects.h
+++ bin/RegisterTritonDialects.h
@@ -59,6 +59,7 @@ inline void registerTritonDialects(mlir::DialectRegistry &registry) {
   mlir::registerTritonAMDGPUAccelerateMatmul();
   mlir::registerTritonAMDGPUOptimizeEpilogue();
   mlir::registerTritonAMDGPUReorderInstructions();
+  mlir::registerTritonAMDGPUHoistReduction();
   mlir::registerTritonAMDGPUStreamPipeline();
   mlir::registerTritonAMDGPUStreamPipelineV2();
   mlir::registerTritonAMDGPUCanonicalizePointers();
