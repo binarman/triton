84777d48880ebbb6c591bb98b0422f3e09561ae8
Alexander Efimov <efimov.alexander@gmail.com>
2025-02-20 14:51:21 +0000

    fix lds optimization crash due to unexpected linear layout

--- third_party/amd/lib/TritonAMDGPUToLLVM/OptimizeLDSUtility.cpp
+++ third_party/amd/lib/TritonAMDGPUToLLVM/OptimizeLDSUtility.cpp
@@ -76,6 +76,9 @@ Attribute createTmpLayout(Attribute layout, ArrayRef<unsigned> warpsPerCTA) {
     return triton::gpu::SliceEncodingAttr::get(
         ctx, src.getDim(), createTmpLayout(src.getParent(), parentWarpsPerCTA));
   }
+  // ignore linear layout in this pattern
+  if (auto src = dyn_cast<triton::gpu::LinearEncodingAttr>(layout))
+    return Attribute();
   assert("Encountered unsupported layout");
   return Attribute();
 }
