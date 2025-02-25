93489980ea7c1083b405ca3c4e22e64dc6509db5
ravil-mobile <ravil.aviva.com@gmail.com>
2025-02-25 13:30:34 +0000

    [AMD] improved canonicalizer for extract and concat ops

--- third_party/amd/lib/Dialect/TritonAMDGPU/IR/Dialect.cpp
+++ third_party/amd/lib/Dialect/TritonAMDGPU/IR/Dialect.cpp
@@ -461,6 +461,27 @@ mlir::LogicalResult foldConcatOpFromSingleSource(amdgpu::ConcatOp op,
   return failure();
 }
 
+struct CanonicalizeConcatOp : public mlir::OpRewritePattern<amdgpu::ConcatOp> {
+  using OpRewritePattern::OpRewritePattern;
+
+  mlir::LogicalResult
+  matchAndRewrite(amdgpu::ConcatOp op,
+                  PatternRewriter &rewriter) const override {
+
+    auto result = op.getResult();
+    auto sources = op.getSources();
+    auto offsets = op.getCoords();
+    if (sources.size() == 1) {
+      assert(product(offsets) == 1);
+      auto source = sources.front();
+      result.replaceAllUsesWith(source);
+      return success();
+    }
+
+    return failure();
+  }
+};
+
 void ConcatOp::getCanonicalizationPatterns(mlir::RewritePatternSet &patterns,
                                            mlir::MLIRContext *context) {
   patterns.add(foldConcatOpFromSingleSource);
