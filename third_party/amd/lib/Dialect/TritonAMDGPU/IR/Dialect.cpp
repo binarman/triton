d4805d122452437461da688f02813ce71e51fc8c
ravil-mobile <ravil.aviva.com@gmail.com>
2025-06-23 11:19:38 +0000

    [AMD] Added fixes to refine-ops-pass after rebase with upstream

--- third_party/amd/lib/Dialect/TritonAMDGPU/IR/Dialect.cpp
+++ third_party/amd/lib/Dialect/TritonAMDGPU/IR/Dialect.cpp
@@ -470,12 +470,16 @@ struct CanonicalizeConcatOp : public mlir::OpRewritePattern<amdgpu::ConcatOp> {
 
     auto result = op.getResult();
     auto sources = op.getSources();
-    auto offsets = op.getCoords();
     if (sources.size() == 1) {
-      assert(product(offsets) == 1);
-      auto source = sources.front();
-      result.replaceAllUsesWith(source);
-      return success();
+      auto resultShape =
+          cast<RankedTensorType>(op.getResult().getType()).getShape();
+      auto sourceShape =
+          cast<RankedTensorType>(sources.front().getType()).getShape();
+      if (resultShape.equals(sourceShape)) {
+        auto source = sources.front();
+        result.replaceAllUsesWith(source);
+        return success();
+      }
     }
 
     return failure();
