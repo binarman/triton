729690afe6b395eeac2c521c6530d79ac6d5e08d
Alexander Efimov <efimov.alexander@gmail.com>
2025-07-22 15:56:10 +0000

    beatify pading printing, print vgprs count

--- analyze_bench.py
+++ analyze_bench.py
@@ -13,7 +13,7 @@ counters = {}
 configs = []
 # dict of config id -> [best bank conflict config, best align stalls, best config with sum of conflicts and stalls]
 best_configs = {}
-with open('mi350_bench.csv') as csvfile:
+with open('mi300_bench.csv') as csvfile:
     reader = csv.reader(csvfile, delimiter=',', quotechar='"')
     # table contents:
     # "Kernel_Name", "VGPR_Count", "SGPR_Count", "Counter_Name", "Counter_Value"
@@ -22,32 +22,40 @@ with open('mi350_bench.csv') as csvfile:
         kernel_name = row[0]
         config_id, padding_config = kernel_name.split('__')
         config_id = int(config_id)
+
+        pad_components = padding_config.split('_')
+        padding_config = ",".join([tup[0] + ":+" + tup[1] for tup in zip(pad_components[::2], pad_components[1::2])])
+
         vgprs = int(row[1])
         sgprs = int(row[2])
         counter_name = row[3]
         counter_val = float(row[4])
-        counters[(config_id, padding_config, counter_name)] = counter_val
+        counters[(config_id, padding_config, counter_name)] = (counter_val, vgprs)
         if (config_id, padding_config) not in configs:
             configs += [(config_id, padding_config)]
     for config_id, padding_config in configs:
         if config_id not in best_configs:
-            best_configs[config_id] = [["", 1e10], ["", 1e10], ["", 1e10]]
-        conflicts = counters[(config_id, padding_config, "SQ_LDS_BANK_CONFLICT")]
-        stalls = counters[(config_id, padding_config, "SQ_LDS_UNALIGNED_STALL")]
-        total = conflicts + stalls
-        if best_configs[config_id][0][1] == conflicts:
-            best_configs[config_id][0][0] += [padding_config]
-        if best_configs[config_id][1][1] == stalls:
-            best_configs[config_id][1][0] += [padding_config]
-        if best_configs[config_id][2][1] == total:
-            best_configs[config_id][2][0] += [padding_config]
+            best_configs[config_id] = {"conflict": ["", 1e10], "stall": ["", 1e10], "total": ["", 1e10]}
+        conflicts = counters[(config_id, padding_config, "SQ_LDS_BANK_CONFLICT")][0]
+        stalls = counters[(config_id, padding_config, "SQ_LDS_UNALIGNED_STALL")][0]
 
-        if best_configs[config_id][0][1] > conflicts:
-            best_configs[config_id][0] = [[padding_config], conflicts]
-        if best_configs[config_id][1][1] > stalls:
-            best_configs[config_id][1] = [[padding_config], stalls]
-        if best_configs[config_id][2][1] > total:
-            best_configs[config_id][2] = [[padding_config], total]
+        padding_config = padding_config + " vgprs:" + str(
+            counters[(config_id, padding_config, "SQ_LDS_UNALIGNED_STALL")][1])
+
+        total = conflicts + stalls
+        if best_configs[config_id]["conflict"][1] == conflicts:
+            best_configs[config_id]["conflict"][0] += [padding_config]
+        if best_configs[config_id]["stall"][1] == stalls:
+            best_configs[config_id]["stall"][0] += [padding_config]
+        if best_configs[config_id]["total"][1] == total:
+            best_configs[config_id]["total"][0] += [padding_config]
+
+        if best_configs[config_id]["conflict"][1] > conflicts:
+            best_configs[config_id]["conflict"] = [[padding_config], conflicts]
+        if best_configs[config_id]["stall"][1] > stalls:
+            best_configs[config_id]["stall"] = [[padding_config], stalls]
+        if best_configs[config_id]["total"][1] > total:
+            best_configs[config_id]["total"] = [[padding_config], total]
     for config in best_configs:
         # print(config_info[config], "bank conflicts:", best_configs[config][0], "stalls:", best_configs[config][1], "total cycles:", best_configs[config][2])
-        print(config_info[config], "total cycles:", best_configs[config][2])
+        print(config_info[config], "total cycles:", best_configs[config]["total"])
