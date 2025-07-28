dace1b5fc9338ece81c550b7db1bf48516d0fab7
Alexander Efimov <efimov.alexander@gmail.com>
2025-07-28 12:10:25 +0000

    simplify slicing test

--- test_3d_slicing.py
+++ test_3d_slicing.py
@@ -18,49 +18,48 @@ def test():
     #smem = #ttg.shared_memory
     #shared = #ttg.padded_shared<[32:+4, 512:+4] {{order = [0, 1]}}>
     #blocked = #ttg.blocked<{{sizePerThread = [1, 8], threadsPerWarp = [4, 16], warpsPerCTA = [1, 1], order = [1, 0]}}>
-    #slice0 = #ttg.slice<{{dim = 0, parent = #blocked}}>
-    #slice1 = #ttg.slice<{{dim = 1, parent = #blocked}}>
+    #blocked1 = #ttg.blocked<{{sizePerThread = [1,1,8], threadsPerWarp = [1,4,16], warpsPerCTA = [1,1,1], order = [2,1,0]}}>
+    #blocked2 = #ttg.blocked<{{sizePerThread = [8], threadsPerWarp = [64], warpsPerCTA = [1], order = [0]}}>
+
     module attributes {{"ttg.num-ctas" = 1 : i32, "ttg.num-warps" = 1 : i32, ttg.target = "hip:gfx942", "ttg.threads-per-warp" = 64 : i32}} {{
         tt.func public @kernel(%x: !tt.ptr<i16> {{tt.divisibility = 16 : i32}}) {{
             %c{h}_i32 = arith.constant {h} : i32
-            %0 = ttg.local_alloc : () -> !ttg.memdesc<{b}x{w}x{h}xi16, #shared, #smem, mutable>
+            %buf = ttg.local_alloc : () -> !ttg.memdesc<{b}x{w}x{h}xi16, #shared, #smem, mutable>
             %c0_i32 = arith.constant 0 : i32
             %c1_i32 = arith.constant 1 : i32
             %c2_i32 = arith.constant 2 : i32
             %c3_i32 = arith.constant 3 : i32
-            %buf_0 = ttg.memdesc_index %0, %c0_i32 : !ttg.memdesc<{b}x{w}x{h}xi16, #shared, #smem, mutable> -> !ttg.memdesc<{w}x{h}xi16, #shared, #smem, mutable>
-            %buf_1 = ttg.memdesc_index %0, %c1_i32 : !ttg.memdesc<{b}x{w}x{h}xi16, #shared, #smem, mutable> -> !ttg.memdesc<{w}x{h}xi16, #shared, #smem, mutable>
-            %buf_2 = ttg.memdesc_index %0, %c2_i32 : !ttg.memdesc<{b}x{w}x{h}xi16, #shared, #smem, mutable> -> !ttg.memdesc<{w}x{h}xi16, #shared, #smem, mutable>
-            %buf_3 = ttg.memdesc_index %0, %c3_i32 : !ttg.memdesc<{b}x{w}x{h}xi16, #shared, #smem, mutable> -> !ttg.memdesc<{w}x{h}xi16, #shared, #smem, mutable>
+            %buf_0 = ttg.memdesc_index %buf, %c0_i32 : !ttg.memdesc<{b}x{w}x{h}xi16, #shared, #smem, mutable> -> !ttg.memdesc<{w}x{h}xi16, #shared, #smem, mutable>
+            %buf_1 = ttg.memdesc_index %buf, %c1_i32 : !ttg.memdesc<{b}x{w}x{h}xi16, #shared, #smem, mutable> -> !ttg.memdesc<{w}x{h}xi16, #shared, #smem, mutable>
+            %buf_2 = ttg.memdesc_index %buf, %c2_i32 : !ttg.memdesc<{b}x{w}x{h}xi16, #shared, #smem, mutable> -> !ttg.memdesc<{w}x{h}xi16, #shared, #smem, mutable>
+            %buf_3 = ttg.memdesc_index %buf, %c3_i32 : !ttg.memdesc<{b}x{w}x{h}xi16, #shared, #smem, mutable> -> !ttg.memdesc<{w}x{h}xi16, #shared, #smem, mutable>
 
+            %cm1_i16 = arith.constant -1 : i16
             %c0_i16 = arith.constant 0 : i16
             %c1_i16 = arith.constant 1 : i16
             %c2_i16 = arith.constant 2 : i16
             %c3_i16 = arith.constant 3 : i16
 
+            %cm1_splat = tt.splat %cm1_i16 : i16 -> tensor<{b}x{w}x{h}xi16, #blocked1>
             %c0_splat = tt.splat %c0_i16 : i16 -> tensor<{w}x{h}xi16, #blocked>
             %c1_splat = tt.splat %c1_i16 : i16 -> tensor<{w}x{h}xi16, #blocked>
             %c2_splat = tt.splat %c2_i16 : i16 -> tensor<{w}x{h}xi16, #blocked>
             %c3_splat = tt.splat %c3_i16 : i16 -> tensor<{w}x{h}xi16, #blocked>
 
-            //ttg.local_store %c0_splat, %buf_0 : tensor<{w}x{h}xi16, #blocked> -> !ttg.memdesc<{w}x{h}xi16, #shared, #smem, mutable, 4x32x128>
-            ttg.local_store %c0_splat, %buf_0 : tensor<{w}x{h}xi16, #blocked> -> !ttg.memdesc<{w}x{h}xi16, #shared, #smem, mutable>
+            ttg.local_store %cm1_splat, %buf : tensor<{b}x{w}x{h}xi16, #blocked1> -> !ttg.memdesc<{b}x{w}x{h}xi16, #shared, #smem, mutable>
+
+            // ttg.local_store %c0_splat, %buf_0 : tensor<{w}x{h}xi16, #blocked> -> !ttg.memdesc<{w}x{h}xi16, #shared, #smem, mutable>
             ttg.local_store %c1_splat, %buf_1 : tensor<{w}x{h}xi16, #blocked> -> !ttg.memdesc<{w}x{h}xi16, #shared, #smem, mutable>
-            ttg.local_store %c2_splat, %buf_2 : tensor<{w}x{h}xi16, #blocked> -> !ttg.memdesc<{w}x{h}xi16, #shared, #smem, mutable>
-            ttg.local_store %c3_splat, %buf_3 : tensor<{w}x{h}xi16, #blocked> -> !ttg.memdesc<{w}x{h}xi16, #shared, #smem, mutable>
+            // ttg.local_store %c2_splat, %buf_2 : tensor<{w}x{h}xi16, #blocked> -> !ttg.memdesc<{w}x{h}xi16, #shared, #smem, mutable>
+            // ttg.local_store %c3_splat, %buf_3 : tensor<{w}x{h}xi16, #blocked> -> !ttg.memdesc<{w}x{h}xi16, #shared, #smem, mutable>
 
-            %buf1_data = ttg.local_load %buf_1 : !ttg.memdesc<{w}x{h}xi16, #shared, #smem, mutable> -> tensor<{w}x{h}xi16, #blocked>
+            // %buf1_data = ttg.local_load %buf_1 : !ttg.memdesc<{w}x{h}xi16, #shared, #smem, mutable> -> tensor<{w}x{h}xi16, #blocked>
 
-            %23 = tt.make_range {{end = {h} : i32, start = 0 : i32}} : tensor<{h}xi32, #slice0>
-            %46 = tt.make_range {{end = {w} : i32, start = 0 : i32}} : tensor<{w}xi32, #slice1>
-            %47 = tt.expand_dims %46 {{axis = 1 : i32}} : tensor<{w}xi32, #slice1> -> tensor<{w}x1xi32, #blocked>
-            %48 = tt.splat %c{h}_i32 : i32 -> tensor<{w}x1xi32, #blocked>
-            %49 = arith.muli %47, %48 : tensor<{w}x1xi32, #blocked>
-            %50 = tt.broadcast %49 : tensor<{w}x1xi32, #blocked> -> tensor<{w}x{h}xi32, #blocked>
-            %51 = tt.expand_dims %23 {{axis = 0 : i32}} : tensor<{h}xi32, #slice0> -> tensor<1x{h}xi32, #blocked>
-            %52 = tt.broadcast %51 : tensor<1x{h}xi32, #blocked> -> tensor<{w}x{h}xi32, #blocked>
-            %53 = arith.addi %52, %50 : tensor<{w}x{h}xi32, #blocked>
-            amdgpu.buffer_store %buf1_data, %x[%53]: tensor<{w}x{h}xi16, #blocked>
+            %buf_data = ttg.local_load %buf : !ttg.memdesc<{b}x{w}x{h}xi16, #shared, #smem, mutable> -> tensor<{b}x{w}x{h}xi16, #blocked1>
+            %buf_lin_data = tt.reshape %buf_data : tensor<{b}x{w}x{h}xi16, #blocked1> -> tensor<{b*w*h}xi16, #blocked2>
+
+            %offset = tt.make_range {{end = {b*w*h} : i32, start = 0 : i32}} : tensor<{b*w*h}xi32, #blocked2>
+            amdgpu.buffer_store %buf_lin_data, %x[%offset]: tensor<{b*w*h}xi16, #blocked2>
             tt.return
         }}
     }}
