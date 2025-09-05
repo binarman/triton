3366d6c2f866d073df8e01d10755c6381baa92b0
Alexander Efimov <efimov.alexander@gmail.com>
2025-09-05 21:14:32 +0000

    fix

--- python/test/gluon/test_core.py
+++ python/test/gluon/test_core.py
@@ -813,7 +813,7 @@ def test_2d_tensor_early_return():
     assert compiled_kernel.asm["llir"].count("define") == 1
 
 
-@pytest.mark.parametrize("interval_pairs", [[32, 1]])
+@pytest.mark.parametrize("interval_pairs", [[[32, 1]]])
 @pytest.mark.parametrize("slice_m_offset, slice_n_offset, slice_m, slice_n", [(48, 16, 16, 16), (32, 48, 32, 16)])
 def test_padded_shared_layout_subslice(interval_pairs, slice_m_offset, slice_n_offset, slice_m, slice_n):
     m = 64
