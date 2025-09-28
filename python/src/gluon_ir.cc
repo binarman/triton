8fc97ce00fa928da6edfe17871fff92be1e29e9d
Dewei Wang <Dewei.Wang@amd.com>
2025-09-28 08:07:06 +0000

    [Gluon Ext] expose sched_barrier, sched_group_barrier

--- python/src/gluon_ir.cc
+++ python/src/gluon_ir.cc
@@ -2,6 +2,7 @@
 #include "pybind11/pybind11.h"
 #include <pybind11/stl.h>
 
+#include "mlir/Dialect/LLVMIR/ROCDLDialect.h"
 #include "mlir/IR/BuiltinTypes.h"
 #include "mlir/IR/Types.h"
 #include "third_party/amd/include/Dialect/TritonAMDGPU/IR/Dialect.h"
@@ -23,6 +24,7 @@ namespace ttg = triton::gpu;
 namespace ttng = triton::nvidia_gpu;
 namespace gluon = mlir::triton::gluon;
 namespace ttag = mlir::triton::amdgpu;
+namespace rocdl = mlir::ROCDL;
 
 // Helper to check if an MLIR type or attribute has a verifier method.
 template <typename AttrOrType>
@@ -798,6 +800,14 @@ void init_gluon_ir(py::module &&m) {
       .def("create_async_tdm_wait", [](GluonOpBuilder &self, int num) {
         ValueRange tokens;
         self.create<ttag::AsyncTDMWait>(tokens, num);
+           })
+      .def("create_sched_barrier",
+           [](GluonOpBuilder &self, unsigned mask) {
+             self.create<rocdl::SchedBarrier>(mask);
+           })
+      .def("create_sched_group_barrier", [](GluonOpBuilder &self, unsigned mask,
+                                            unsigned size, unsigned groupId) {
+        self.create<rocdl::SchedGroupBarrier>(mask, size, groupId);
       });
 
   py::class_<ttg::WarpSpecializeOp, OpState>(m, "WarpSpecializeOp",
--- python/triton/experimental/gluon/language/amd/cdna3/__init__.py
+++ python/triton/experimental/gluon/language/amd/cdna3/__init__.py
@@ -11,7 +11,7 @@ if TYPE_CHECKING:
 
 __all__ = [
     "buffer_atomic_add", "buffer_atomic_and", "buffer_atomic_min", "buffer_atomic_max", "buffer_atomic_or",
-    "buffer_atomic_xor", "buffer_atomic_xor", "buffer_load", "buffer_store", "mfma"
+    "buffer_atomic_xor", "buffer_atomic_xor", "buffer_load", "buffer_store", "mfma", "sched_barrier", "sched_group_barrier"
 ]
 
 _atomic_op_str_to_op = {
@@ -236,3 +236,26 @@ def buffer_atomic_xchg(ptr, offsets, value, mask=None, sem=None, scope=None, _se
 
     return _buffer_atomic_rmw_impl('xchg', ptr, offsets, value, "cdna3", mask=mask, sem=sem, scope=scope,
                                    _semantic=_semantic)
+@builtin
+def sched_barrier(mask, _semantic: GluonSemantic = None):
+    """
+    sched_barrier to help instruction scheduling
+    Args:
+        mask (i32): mask for the types of instructions that may be allowed to cross the SCHED_BARRIER during scheduling
+    """
+    mask = ttgl._unwrap_if_constexpr(mask)
+    _semantic.builder.create_sched_barrier(mask)
+
+@builtin
+def sched_group_barrier(mask, size, group_id, _semantic: GluonSemantic = None):
+    """
+    sched_group_barrier to help instruction scheduling
+    Args:
+        mask (i32): the type of instructions to synchronize around(0x008:MFMA, 0x020:VMEM, 0x100:LDS_READ, 0x200:DS_WRITE)
+        size (i32): the number of matching instructions associated with this barrier
+        group_id (i32): what other sched_group_barriers to synchronize with
+    """
+    mask = ttgl._unwrap_if_constexpr(mask)
+    size = ttgl._unwrap_if_constexpr(size)
+    group_id = ttgl._unwrap_if_constexpr(group_id)
+    _semantic.builder.create_sched_group_barrier(mask, size, group_id)
