9aaffaf6d68df9ca4a304bac4c0834c9b5f05e95
Alexander Efimov <efimov.alexander@gmail.com>
2025-10-06 14:49:45 +0000

    rebase decompose optimization and add a reproducer

---
+++ reproduce.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/bash
+pytest python/triton_kernels/tests/test_matmul.py::test_op[False-False-True-True-False-None-128-300-400-400-ragged-bfloat16-mxfloat8_e4m3fn-8-4-1-False-None-False-False-False-True]
