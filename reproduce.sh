97a16ac5ac5a8f4379a51d5cf8fb1abc079d7b65
Alexander Efimov <efimov.alexander@gmail.com>
2025-10-06 15:00:09 +0000

    fix reproducer

--- reproduce.sh
+++ reproduce.sh
@@ -1,2 +1,2 @@
-#!/usr/bin/python3
-pytest python/triton_kernels/tests/test_matmul.py::test_op[False-False-True-True-False-128-300-400-400-ragged-bfloat16-mxfloat8_e4m3fn-8-4-1-1-False-None]
+#!/usr/bin/bash
+pytest python/triton_kernels/tests/test_matmul.py::test_op[False-False-True-True-False-None-128-300-400-400-ragged-bfloat16-mxfloat8_e4m3fn-8-4-1-False-None-False-False-False-True]
