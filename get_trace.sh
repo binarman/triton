6b1258b0ae37a47653c7b5a3a7eaba18287ebb95
Alexander Efimov <efimov.alexander@gmail.com>
2026-02-27 16:11:30 +0000

    change rocprof config files

--- get_trace.sh
+++ get_trace.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
 rm -rf sm_trace
-ROCPROF_ATT_LIBRARY_PATH=/rocprof-decoder/rocprof-trace-decoder-manylinux-2.28-0.1.6-Linux/opt/rocm/lib/ rocprofv3 --att -i att.json -d sm_trace -- python skinny_matmul/utils/03-matrix-multiplication.py
+ROCPROF_ATT_LIBRARY_PATH=/rocprof-decoder/rocprof-trace-decoder-manylinux-2.28-0.1.6-Linux/opt/rocm/lib/ rocprofv3 --att -i att.json -d sm_trace -- python skinny_matmul/run_all.py
 tar -cf sm_trace.tar sm_trace/ui_output*
 mv sm_trace.tar /host/
